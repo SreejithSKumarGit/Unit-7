42e960f109bcf041c904dae9cec2e9c5107a2a5a
Sreejith S Kumar <sreejithskumar80@gmail.com>
2022-07-16 22:11:08 +0530

    Movie Store API

--- Movie_Store_API/Handlers/movieHandler.js
+++ Movie_Store_API/Handlers/movieHandler.js
@@ -1,3 +1,4 @@
+const { rawListeners } = require("../Database/Moviesdb");
 const Movie=require("../Database/Moviesdb");
 
 async function fetchAllMovies(req,res,next)
@@ -8,6 +9,86 @@ async function fetchAllMovies(req,res,next)
         data:movies
     })
 }
+async function createMovieData(req,res,next)
+{
+    const data=req.body;
+    await Movie.create(data);
+    res.send(data);
+}
+async function updateMovieData(req,res,next)
+{
+    const {id}=req.params;
+    const body=req.body;
+    let data=await Movie.findOneAndUpdate({_id:id},body);
+    res.send(data);
+
+}
+async function deleteMovieData(req,res,next)
+{
+    const{id}=req.params;
+    await Movie.findOneAndDelete({_id:id});
+    res.send("Movie Data has been deleted successfully");
+}
+async function filterByTitle(req,res,next)
+{
+    const{title}=req.params;
+    
+    const data=await Movie.find({title:title});
+    res.send(data);
+}
+async function filterByRating(req,res,next)
+{
+    const{rating}=req.params;
+    
+    const data =await Movie.find({vote_average:rating});
+    res.send(data);
+
+}
+async function searchMovie(req,res,next)
+{
+    const {search}=req.params;
+
+    const data=await Movie.find({title:{$regex:`^${search}`,$options:"$i"}});
+
+    res.send(data);
+}
+async function sortMovie(req,res,next)
+{
+    const {sortfield}=req.params;
+    let sortValue;
+   if(sortfield==="vote_average")
+   {
+     sortValue=
+    {
+        vote_average:1
+    }
+   }
+   else if(sortfield==="vote_count")
+   {
+    sortValue=
+    {
+        vote_count:1
+    }
+   }
+   else if(sortfield==="id")
+   {
+    sortValue=
+    {
+        id:1
+    }
+   }
+    
+    
+    const data=await Movie.find().sort(sortValue);
+    res.send(data);
+}
 module.exports={
     fetchAllMovies,
+    createMovieData,
+    filterByTitle,
+    filterByRating,
+    searchMovie,
+    sortMovie,
+    updateMovieData,
+    deleteMovieData
 }
--- Movie_Store_API/Router/movieRouter.js
+++ Movie_Store_API/Router/movieRouter.js
@@ -1,8 +1,16 @@
+const { Router } = require("express");
 const express=require("express");
-const{fetchAllMovies}=require("../Handlers/movieHandler");
+const{fetchAllMovies, createMovieData, filterByTitle, filterByRating, searchMovie, sortMovie, updateMovieData, deleteMovieData}=require("../Handlers/movieHandler");
 const movieRouter=express.Router();
 
 
 movieRouter.get("/movies",fetchAllMovies);
+movieRouter.post("/movies",createMovieData);
+movieRouter.get(`/movie/title=:title`,filterByTitle);
+movieRouter.get(`/movie/rating:rating`,filterByRating);
+movieRouter.get('/movie/search=:search',searchMovie);
+movieRouter.get("/movie/sortBy=:sortfield",sortMovie);
+movieRouter.post("/movie/updateByID=:id",updateMovieData);
+movieRouter.delete("/movie/deleteByID=:id",deleteMovieData);
 
 module.exports=movieRouter;
